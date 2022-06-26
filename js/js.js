7a1a0643bccdcd38d9b2648fa618da53669d1c70
hooman <hoomanmousavi77@gmail.com>
2022-06-26 22:59:54 +0430

    Upload simple PWA project on Github

---
+++ js/js.js
@@ -0,0 +1,13 @@
+const textTag=document.getElementById('text')
+function touchstartHandler() {
+    alert('touch start handler')
+}
+function touchmoveHandler() {
+    alert('touch move handler')
+}
+function touchendHandler() {
+    alert('touch end handler')
+}
+textTag.addEventListener('touchstart',touchstartHandler)
+textTag.addEventListener('touchend',touchendHandler)
+textTag.addEventListener('touchmove',touchmoveHandler)
